26472e42db9c9623092bba82ce8216870cddd086
Mihai Parparita <mihai@persistent.info>
2021-06-18 15:12:51 -0700

    Transparent Mac boilerplate
    create-react-app TypeScript, Cloudflare Worker Sites, and prettier
    setup.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>Transparent Mac</p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
