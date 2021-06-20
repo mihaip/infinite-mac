685d22f501325d7c15ecd908211c42fed961cd33
Mihai Parparita <mihai@persistent.info>
2021-06-19 17:07:01 -0700

    Begin import of Basilisk II
    Adds support JS files and generated wasm from https://github.com/mihaip/macemu.
    Not actual hooked up, though there is a placeholder <canvas> in the HTML
    now.
    
    Also includes basic support for serving the ROM and disk images from
    Cloudflare Workers. There's a 25MB limit (because the data ends up
    in the KV store), thus we need to store them gzip-ed (and convince
    Cloudflare to pass through the data and let the browser decompress it).

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="react-scripts" />
+
+declare module "*.txt" {
+    const path: string;
+    export default path;
+}
+
+declare module "*.gz" {
+    const path: string;
+    export default path;
+}
