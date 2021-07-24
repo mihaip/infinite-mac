38d4cced481ec26c3d91807d41b954d9f0826dc6
Mihai Parparita <mihai@persistent.info>
2021-07-24 16:39:19 -0700

    Tweak name

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import {render, screen} from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders something", () => {
     render(<App />);
-    const titleElement = screen.getByText(/Transparent Mac/i);
+    const titleElement = screen.getByText(/Inside Macintosh/i);
     expect(titleElement).toBeInTheDocument();
 });
