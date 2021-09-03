959281d1c2ecb4200f6ea71ec445e7b45a020f6c
Mihai Parparita <mihai@persistent.info>
2021-09-03 12:58:37 -0700

    Make Basilisk II source maps work
    We needed to serve them under the URL /BasiliskII.wasm.map (thus the
    file needs to live in the /public directory). Additionally, the
    paths that it contained were relative to the BasiliskII/src/Unix
    directory, change them to absolute file:/// URLs instead.

--- scripts/import-basilisk-ii.sh
+++ scripts/import-basilisk-ii.sh
@@ -7,13 +7,12 @@ ROOT_DIR="`dirname "${BASH_SOURCE[0]}"`/.."
 BASILISK_II_DIR="${ROOT_DIR}/../macemu/BasiliskII/src/Unix"
 SRC_DIR="${ROOT_DIR}/src"
 DATA_DIR="${SRC_DIR}/Data"
+PUBLIC_DIR="${ROOT_DIR}/public"
 BASILISK_II_DESTINATION_DIR="${SRC_DIR}/BasiliskII"
 
 # Build output
 cp "${BASILISK_II_DIR}/BasiliskII" "${BASILISK_II_DESTINATION_DIR}/BasiliskII.jsz"
 cp "${BASILISK_II_DIR}/BasiliskII.wasm" "${BASILISK_II_DESTINATION_DIR}/BasiliskII.wasmz"
-cp "${BASILISK_II_DIR}/BasiliskII.wasm.map" "${BASILISK_II_DESTINATION_DIR}/BasiliskII.wasm.map"
-
-# ROM and disk images
-brotli -q 11 -k "${BASILISK_II_DIR}/Quadra-650.rom" "${BASILISK_II_DIR}/Macintosh HD.dsk"
-mv "${BASILISK_II_DIR}/Quadra-650.rom.br" "${BASILISK_II_DIR}/Macintosh HD.dsk.br" "${DATA_DIR}/"
+# Source map needs massaging and needs to be put into the public directory (we
+# don't control the URL that it's referenced under).
+scripts/rewrite-basilisk-ii-source-map.py "${BASILISK_II_DIR}/BasiliskII.wasm.map" "${PUBLIC_DIR}/BasiliskII.wasm.map"
---
+++ scripts/rewrite-basilisk-ii-source-map.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+import json
+import os
+import sys
+
+input_path = sys.argv[1]
+output_path = sys.argv[2]
+
+print input_path, output_path
+
+with open(input_path, "r") as input_file:
+    source_map = json.load(input_file)
+
+base_dir = os.path.dirname(input_path)
+rewritten_sources = []
+for path in source_map["sources"]:
+    abs_path = os.path.abspath(os.path.join(base_dir, path))
+    if os.path.exists(abs_path):
+        rewritten_sources.append("file://" + abs_path)
+    else:
+        # Some paths are from the Emscripten SDK and are not actually on
+        # disk (they are instead in the Emscripten Docker image). Leave those
+        # alone.
+        rewritten_sources.append(path)
+source_map["sources"] = rewritten_sources
+
+with open(output_path, "w+") as output_file:
+    json.dump(source_map, output_file)
