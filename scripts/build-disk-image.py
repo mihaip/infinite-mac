b393a95ff9338f98b9cc2d1fab73c18bbca055cb
Mihai Parparita <mihai@persistent.info>
2021-10-03 17:48:54 -0700

    Include extracted .zip files in built disk image
    Extends the approach from 82a8a099116970336791163b14f1983b630ab9d1 for extracted
    .zips too -- the .finf and .rsrc files that ExtFS generates are copied into
    the resource fork and Finder metadata fields respectively.
    
    Photoshop and Kaleidoscope are currently not included because they contain
    malformed file names (they were generated before proper MacRoman encoding
    in mihaip/macemu@80e1038734b1f78980b0315c2ca1483f3293561a). They will most
    likely need to be re-generated.

--- scripts/build-disk-image.py
+++ scripts/build-disk-image.py
@@ -10,6 +10,7 @@ import struct
 import sys
 import typing
 import urllib.request
+import zipfile
 
 LIBRARY_DIR = os.path.join(os.path.dirname(__file__), "..", "Library")
 CACHE_DIR = os.path.join("/tmp", "infinite-mac-cache")
@@ -18,7 +19,7 @@ input_path = sys.argv[1]
 output_dir = sys.argv[2]
 manifest_dir = sys.argv[3]
 
-DISK_SIZE = 100 * 1024 * 1024
+DISK_SIZE = 200 * 1024 * 1024
 CHUNK_SIZE = 256 * 1024
 chunk_count = 0
 total_size = 0
@@ -41,8 +42,14 @@ def read_url(url: str) -> bytes:
 
 
 def get_import_folders() -> typing.Dict[str, machfs.Folder]:
-    sys.stderr.write("Importing other images\n")
+    import_folders = {}
+    import_folders.update(import_manifests())
+    import_folders.update(import_zips())
+    return import_folders
 
+
+def import_manifests() -> typing.Dict[str, machfs.Folder]:
+    sys.stderr.write("Importing other images\n")
     import_folders = {}
     for manifest_path in glob.iglob(os.path.join(LIBRARY_DIR, "**", "*.json")):
         folder_path, _ = os.path.splitext(
@@ -72,10 +79,83 @@ def get_import_folders() -> typing.Dict[str, machfs.Folder]:
             assert False, "Unexpected manifest format: %s" % json.dumps(
                 manifest_json)
         import_folders[folder_path] = folder
-        sys.stderr.flush()
-
-    sys.stderr.write("\n")
     return import_folders
+
+
+def import_zips() -> typing.Dict[str, machfs.Folder]:
+    sys.stderr.write("Importing .zips\n")
+    import_folders = {}
+
+    for zip_path in glob.iglob(os.path.join(LIBRARY_DIR, "**", "*.zip")):
+        folder_path, _ = os.path.splitext(
+            os.path.relpath(zip_path, LIBRARY_DIR))
+        sys.stderr.write("  Importing %s\n" % folder_path)
+
+        folder = machfs.Folder()
+        files_by_path = {}
+        with zipfile.ZipFile(zip_path, "r") as zip:
+            for zip_info in zip.infolist():
+                if zip_info.is_dir():
+                    continue
+                file_data = zip.read(zip_info)
+                if zip_info.filename == "DInfo":
+                    folder.usrInfo = file_data[0:16]
+                    folder.fndrInfo = file_data[16:]
+                    continue
+                path = zip_info.filename
+                if ".rsrc/" in path:
+                    path = path.replace(".rsrc/", "")
+                    files_by_path.setdefault(path,
+                                             machfs.File()).rsrc = file_data
+                    continue
+                if ".finf/" in path:
+                    # May actually be the DInfo for a folder, check for that.
+                    path = path.replace(".finf/", "")
+                    try:
+                        # Will throw if there isn't a corresponding directory,
+                        # no need to actually do anything with the return value.
+                        zip.getinfo(path + "/")
+                        nested_folder_path, nested_folder_name = os.path.split(
+                            path)
+                        parent = traverse_folders(folder, nested_folder_path)
+                        nested_folder = machfs.Folder()
+                        (nested_folder.usrInfo,
+                         nested_folder.fndrInfo) = struct.unpack(
+                             '>16s16s', file_data)
+                        parent[fix_name(nested_folder_name)] = nested_folder
+                        continue
+                    except KeyError:
+                        pass
+                    file = files_by_path.setdefault(path, machfs.File())
+                    (file.type, file.creator, file.flags, file.x, file.y, _,
+                     file.fndrInfo) = struct.unpack('>4s4sHhhH16s', file_data)
+                    continue
+                files_by_path.setdefault(path, machfs.File()).data = file_data
+
+        for path, file in files_by_path.items():
+            file_folder_path, file_name = os.path.split(path)
+            parent = traverse_folders(folder, file_folder_path)
+
+            parent[fix_name(file_name)] = file
+
+        import_folders[folder_path] = folder
+
+    return import_folders
+
+
+def traverse_folders(parent: machfs.Folder, folder_path: str) -> machfs.Folder:
+    if folder_path:
+        folder_path_pieces = folder_path.split(os.path.sep)
+        for folder_path_piece in folder_path_pieces:
+            folder_path_piece = fix_name(folder_path_piece)
+            if folder_path_piece not in parent:
+                parent[folder_path_piece] = machfs.Folder()
+            parent = parent[folder_path_piece]
+    return parent
+
+
+def fix_name(name: str) -> str:
+    return name.replace(":", "/")
 
 
 import_folders = get_import_folders()
@@ -91,11 +171,9 @@ with open(input_path, "rb") as input_file:
     v.name = "Macintosh HD"
 
     for folder_path, folder in import_folders.items():
-        parent = v["Library"]
-        folder_path_pieces = folder_path.split(os.path.sep)
-        for folder_path_piece in folder_path_pieces[:-1]:
-            parent = parent[folder_path_piece]
-        parent[folder_path_pieces[-1]] = folder
+        parent_folder_path, folder_name = os.path.split(folder_path)
+        parent = traverse_folders(v["Library"], parent_folder_path)
+        parent[folder_name] = folder
 
     flat = v.write(
         size=DISK_SIZE,
@@ -107,7 +185,7 @@ with open(input_path, "rb") as input_file:
     for i in range(0, DISK_SIZE, CHUNK_SIZE):
         chunk = flat[i:i + CHUNK_SIZE]
         total_size += len(chunk)
-        chunk_compressed = brotli.compress(chunk, quality=0)  # NOCOMMIT
+        chunk_compressed = brotli.compress(chunk, quality=11)
         chunk_path = os.path.join(output_dir,
                                   f"{input_file_name}.{chunk_count}.br")
         # Use compressed version for the version hash so that if we change the
