de718eb7a2e086a5d47430ee448816fbdde4116d
Mihai Parparita <mihai@persistent.info>
2021-10-15 15:13:40 -0700

    Use Git submodule for the mihaip/macemu dependency instead of assuming that it's in a sibling directory.

--- scripts/import-basilisk-ii-data.sh
+++ scripts/import-basilisk-ii-data.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Assumes that https://github.com/mihaip/macemu is in a sibling directory and
-# it has been built
+# Assumes that the https://github.com/mihaip/macemu submodule has been fetched
+# and it has been built.
 
 ROOT_DIR="`dirname "${BASH_SOURCE[0]}"`/.."
-BASILISK_II_DIR="${ROOT_DIR}/../macemu/BasiliskII/src/Unix"
+BASILISK_II_DIR="${ROOT_DIR}/macemu/BasiliskII/src/Unix"
 DATA_DIR="${ROOT_DIR}/src/Data"
 PUBLIC_DIR="${ROOT_DIR}/public"
 
 # ROM
-#brotli -q 11 -k "${BASILISK_II_DIR}/Quadra-650.rom"
-#mv "${BASILISK_II_DIR}/Quadra-650.rom.br"  "${DATA_DIR}/"
+brotli -q 11 -k "${BASILISK_II_DIR}/Quadra-650.rom"
+mv "${BASILISK_II_DIR}/Quadra-650.rom.br"  "${DATA_DIR}/"
 
 scripts/build-disk-image.py "${BASILISK_II_DIR}/Macintosh HD.dsk" "${PUBLIC_DIR}/Disk" "${DATA_DIR}/"
