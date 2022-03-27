da3fd0c092f7ece06803f759051c51296c4df77c
Mihai Parparita <mihai@persistent.info>
2022-03-27 17:30:21 -0400

    Add footer with link to website.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import {Footer} from "./Footer";
 import {Mac} from "./Mac";
 
 function App() {
     return (
         <div className="App">
             <Mac />
+            <Footer />
         </div>
     );
 }
---
+++ src/Footer.tsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+export function Footer() {
+    return (
+        <div className="Footer">
+            Infinite Mac - A project by{" "}
+            <a href="http://persistent.info">Mihai Parparita</a> -{" "}
+            <a href="https://github.com/mihaip/infinite-mac">Source</a>
+        </div>
+    );
+}
