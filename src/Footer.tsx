0f9342e9df70f55486de12a0150bbb938a197257
Mihai Parparita <mihai@persistent.info>
2022-04-01 20:48:39 -0400

    Add blog post links.

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -5,7 +5,10 @@ export function Footer() {
         <div className="Footer">
             Infinite Mac - A project by{" "}
             <a href="http://persistent.info">Mihai Parparita</a> -{" "}
-            <a href="https://github.com/mihaip/infinite-mac">Source</a>
+            <a href="https://blog.persistent.info/2022/03/blog-post.html">
+                About
+            </a>{" "}
+            - <a href="https://github.com/mihaip/infinite-mac">Source</a>
         </div>
     );
 }
