feeedf200628cb1382b122801a7421d0c9324f6b
Mihai Parparita <mihai@persistent.info>
2022-04-02 16:34:33 -0400

    Be more resilient to Firefox private browsing mode.
    Both IndexedDB and service workers are not available in that case,
    catch the exceptions that would be thrown in that case.

--- src/BasiliskII/emulator-ui-persistence.ts
+++ src/BasiliskII/emulator-ui-persistence.ts
@@ -4,15 +4,25 @@ import * as idbKeyVal from "idb-keyval";
 export async function persistData(
     extraction: EmulatorWorkerDirectorExtraction
 ) {
-    await idbKeyVal.set(EXTRACTION_KEY, extraction);
+    try {
+        await idbKeyVal.set(EXTRACTION_KEY, extraction);
+    } catch (err) {
+        console.warn("Could not persist data", err);
+    }
 }
 
 export async function getPersistedData(): Promise<
     EmulatorWorkerDirectorExtraction | undefined
 > {
-    const extraction = await idbKeyVal.get<EmulatorWorkerDirectorExtraction>(
-        EXTRACTION_KEY
-    );
-    return extraction;
+    try {
+        const extraction =
+            await idbKeyVal.get<EmulatorWorkerDirectorExtraction>(
+                EXTRACTION_KEY
+            );
+        return extraction;
+    } catch (err) {
+        console.warn("Could not read persisted data", err);
+        return undefined;
+    }
 }
 const EXTRACTION_KEY = "extraction";
