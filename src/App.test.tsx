6a3cce3a97f24ce7f2a4c100439515025b293781
Mihai Parparita <mihai@persistent.info>
2022-08-24 21:24:20 -0700

    Trim down npm dependencies a bit
    Also remove the (placeholder) tests.

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import {render, screen} from "@testing-library/react";
-import App from "./App";
-
-test("renders something", () => {
-    render(<App />);
-    const titleElement = screen.getByText(/Infinite Mac/i);
-    expect(titleElement).toBeInTheDocument();
-});
