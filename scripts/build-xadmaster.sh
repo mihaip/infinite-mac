bb76c1596683d49bfc1996d46f487e5012caa5c0
Mihai Parparita <mihai@persistent.info>
2023-02-24 22:06:04 -0800

    Fix creation of XDMaster-build directory

--- scripts/build-xadmaster.sh
+++ scripts/build-xadmaster.sh
@@ -5,8 +5,9 @@
 
 ROOT_DIR="`dirname "${BASH_SOURCE[0]}"`/.."
 XADMASTER_PROJECT="${ROOT_DIR}/XADMaster/XADMaster.xcodeproj"
-XADMASTER_DESTINATION_DIR=`realpath "${ROOT_DIR}/XADMaster-build"`
+XADMASTER_DESTINATION_DIR="${ROOT_DIR}/XADMaster-build"
 
 mkdir -p "${XADMASTER_DESTINATION_DIR}"
+XADMASTER_DESTINATION_DIR=`realpath "${XADMASTER_DESTINATION_DIR}"`
 xcodebuild -scheme lsar -project "${XADMASTER_PROJECT}" -configuration Release SYMROOT="${XADMASTER_DESTINATION_DIR}"
 xcodebuild -scheme unar -project "${XADMASTER_PROJECT}" -configuration Release SYMROOT="${XADMASTER_DESTINATION_DIR}"
