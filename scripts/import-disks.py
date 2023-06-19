0364ba0bc849c1c0b3cd81f2257077e8c5a9b894
Mihai Parparita <mihai@persistent.info>
2023-06-19 12:04:03 -0700

    Bump Infinite HD size to 2GB
    Not because we want to include that much more software on it, but because
    some CD-ROM software needs to be installed (e.g. Riven needs ~100MB),
    and having a scratchpad to install to is useful.
    
    Updates #166

--- scripts/import-disks.py
+++ scripts/import-disks.py
@@ -504,7 +504,7 @@ def build_library_image(base_name: str, dest_dir: str) -> ImageDef:
         parent[folder_name] = folder
 
     image = v.write(
-        size=1000 * 1024 * 1024,
+        size=2000 * 1024 * 1024,
         align=512,
         desktopdb=False,
         bootable=False,
