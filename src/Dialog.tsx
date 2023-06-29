1886bf8e48a74685c336b62c9e634e941ca6b811
Mihai Parparita <mihai@persistent.info>
2023-06-28 22:06:14 -0700

    Add UI for custom CD-ROM URLs
    Updates #166

--- src/Dialog.tsx
+++ src/Dialog.tsx
@@ -8,6 +8,8 @@ export type DialogProps = {
     children: React.ReactNode;
     onDone: () => void;
     doneLabel?: string;
+    doneEnabled?: boolean;
+    onCancel?: () => void;
     buttonAppearance?: ButtonProps["appearance"];
 };
 
@@ -17,6 +19,8 @@ export function Dialog(props: DialogProps) {
         children,
         onDone,
         doneLabel = "Done",
+        doneEnabled = true,
+        onCancel,
         buttonAppearance = "Classic",
     } = props;
     const dialog = (
@@ -26,13 +30,24 @@ export function Dialog(props: DialogProps) {
             {children}
 
             <footer>
+                {onCancel && (
+                    <Button
+                        className="Dialog-Cancel"
+                        appearance={buttonAppearance}
+                        onClick={e => {
+                            e.preventDefault();
+                            onCancel();
+                        }}>
+                        Cancel
+                    </Button>
+                )}
                 <Button
+                    disabled={!doneEnabled}
                     appearance={buttonAppearance}
                     onClick={e => {
                         e.preventDefault();
                         onDone();
-                    }}
-                >
+                    }}>
                     {doneLabel}
                 </Button>
             </footer>
