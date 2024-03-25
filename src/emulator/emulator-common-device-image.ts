4ab972e191e9afc67aee0a7056ac895c78b9721e
Mihai Parparita <mihai@persistent.info>
2024-03-24 21:34:43 -0700

    Add more drivers to the device image headers
    The image added in 910505f65352a82f42765daa39ccacac34c9eba9 only had the
    Apple SCSI 4.3 driver, but we also need the IDE driver for the Beige G3
    and other machines with IDE built-in. Switch out the the device image
    template to one with all the driver partitions that early Mac OS X would
    generate (SCSI, IDE, etc.)
    
    Updates #219
    Updates #234

--- src/emulator/emulator-common-device-image.ts
+++ src/emulator/emulator-common-device-image.ts
@@ -1,13 +1,28 @@
 const BLOCK_SIZE = 512;
 
+// Partition blocks defined in the base header
+// 0: Apple_partition_map
+// 1: Apple_Driver43
+// 2: Apple_Driver43
+// 3: Apple_Driver_ATA
+// 4: Apple_Driver_ATA
+// 5: Apple_FWDriver
+// 6: Apple_Driver_IOKit
+// 7: Apple_Patches
+// 8: Apple_HFS
+const HFS_PARTITION_INDEX = 8;
+
+// +1 to account for for block 0 with the device information
+const HFS_PARTITION_OFFSET = (HFS_PARTITION_INDEX + 1) * BLOCK_SIZE;
+
 export function generateDeviceImageHeader(
     baseHeader: ArrayBuffer,
     hfsPartitionSize: number
 ): ArrayBuffer {
     const headerSize = baseHeader.byteLength;
-    if (headerSize !== 0xc000) {
+    if (headerSize !== 0xe4000) {
         console.warn(
-            "Expected base header to be 0xc000 bytes, got 0x" +
+            "Expected base header to be 0xE4000 bytes, got 0x" +
                 headerSize.toString(16) +
                 " bytes, results may be unpredictable"
         );
@@ -20,8 +35,8 @@ export function generateDeviceImageHeader(
     headerView.setInt32(0x4, totalBlocks); // sbBlkCount
 
     const hfsBlocks = hfsPartitionSize / BLOCK_SIZE;
-    headerView.setInt32(0x060c, hfsBlocks); // pmPartBlkCnt?
-    headerView.setInt32(0x0654, hfsBlocks); // pmDataCnt?
+    headerView.setInt32(HFS_PARTITION_OFFSET + 12, hfsBlocks); // pmPartBlkCnt
+    headerView.setInt32(HFS_PARTITION_OFFSET + 84, hfsBlocks); // pmDataCnt
 
     return header;
 }
