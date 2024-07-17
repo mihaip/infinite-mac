e5b4d2f5cfa146a6a62be2a0c5d7d4210440a4f5
Mihai Parparita <mihai@persistent.info>
2024-07-16 21:45:45 -0700

    Don't use mouse deltas in an emulator that doesn't support them
    Could happen if enabling mouse deltas in a SheepShaver- or Basilisk II-
    based machine and then switching to a Mini vMac-based one.
    
    Addresses the issue discussed by https://github.com/mihaip/infinite-mac/issues/326#issuecomment-2228811727

--- src/emulator/emulator-ui.ts
+++ src/emulator/emulator-ui.ts
@@ -14,6 +14,7 @@ import {
     emulatorUsesPlaceholderDisks,
     emulatorNeedsMouseDeltas,
     type EmulatorDef,
+    emulatorSupportsMouseDeltas,
 } from "./emulator-common-emulators";
 import {getEmulatorWasmPath} from "./emulator-ui-emulators";
 import Worker from "./emulator-worker?worker";
@@ -553,9 +554,11 @@ export class Emulator {
     }
 
     #useMouseDeltas() {
+        const {emulatorType} = this.#config.machine;
         return (
-            emulatorNeedsMouseDeltas(this.#config.machine.emulatorType) ||
-            this.#delegate?.emulatorSettings?.(this).useMouseDeltas === true
+            emulatorNeedsMouseDeltas(emulatorType) ||
+            (this.#delegate?.emulatorSettings?.(this).useMouseDeltas === true &&
+                emulatorSupportsMouseDeltas(emulatorType))
         );
     }
 
